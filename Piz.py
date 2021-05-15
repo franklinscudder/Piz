da182ce9d1db17fa8d04bb48805d9c0e37fb4ee8
Tom Findlay <findlaytel@gmail.com>
2021-05-15 02:25:25 +0100

    Create Piz.py

---
+++ Piz.py
@@ -0,0 +1,14 @@
+
+import os, sys
+
+filename = sys.argv[1]
+depth = int(sys.argv[2])
+
+def convert(string);
+    return "".join([ord(c) for c in string])
+
+with open(filename, "r") as file:
+    data = file.read())
+    print(hex)
+    
+
